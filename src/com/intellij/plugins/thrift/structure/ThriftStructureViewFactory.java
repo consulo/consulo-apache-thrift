45ccd9e094a7d87a397fa1c59dc9ec7bfe47ccd0
VISTALL <vistall.valeriy@gmail.com>
2014-02-08 22:48:46 +0200

    compilation fixed

--- src/com/intellij/plugins/thrift/structure/ThriftStructureViewFactory.java
+++ src/com/intellij/plugins/thrift/structure/ThriftStructureViewFactory.java
@@ -16,28 +16,36 @@
 package com.intellij.plugins.thrift.structure;
 
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import com.intellij.ide.structureView.StructureViewBuilder;
 import com.intellij.ide.structureView.StructureViewModel;
 import com.intellij.ide.structureView.TreeBasedStructureViewBuilder;
 import com.intellij.lang.PsiStructureViewFactory;
+import com.intellij.openapi.editor.Editor;
 import com.intellij.psi.PsiFile;
 
 /**
- * @author: Fedor.Korotkov
+ * @author Fedor.Korotkov
  */
-public class ThriftStructureViewFactory implements PsiStructureViewFactory {
-  @Override
-  public StructureViewBuilder getStructureViewBuilder(final PsiFile psiFile) {
-    return new TreeBasedStructureViewBuilder() {
-      @NotNull
-      public StructureViewModel createStructureViewModel() {
-        return new ThriftStructureViewModel(psiFile);
-      }
+public class ThriftStructureViewFactory implements PsiStructureViewFactory
+{
+	@Override
+	public StructureViewBuilder getStructureViewBuilder(final PsiFile psiFile)
+	{
+		return new TreeBasedStructureViewBuilder()
+		{
+			@Override
+			@NotNull
+			public StructureViewModel createStructureViewModel(@Nullable Editor editor)
+			{
+				return new ThriftStructureViewModel(psiFile);
+			}
 
-      @Override
-      public boolean isRootNodeShown() {
-        return false;
-      }
-    };
-  }
+			@Override
+			public boolean isRootNodeShown()
+			{
+				return false;
+			}
+		};
+	}
 }
