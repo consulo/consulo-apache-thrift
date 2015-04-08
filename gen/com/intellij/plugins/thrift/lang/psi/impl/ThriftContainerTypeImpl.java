3eea0ae69373c2dfbe2b83ffb53d158d461ea1c4
VISTALL <vistall.valeriy@gmail.com>
2015-04-08 14:55:40 +0300

    missed

---
+++ gen/com/intellij/plugins/thrift/lang/psi/impl/ThriftContainerTypeImpl.java
@@ -0,0 +1,49 @@
+// This is a generated file. Not intended for manual editing.
+package com.intellij.plugins.thrift.lang.psi.impl;
+
+import java.util.List;
+import org.jetbrains.annotations.*;
+import com.intellij.lang.ASTNode;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiElementVisitor;
+import com.intellij.psi.util.PsiTreeUtil;
+import static com.intellij.plugins.thrift.lang.lexer.ThriftTokenTypes.*;
+import com.intellij.plugins.thrift.lang.psi.*;
+import com.intellij.plugins.thrift.util.ThriftPsiUtil;
+
+public class ThriftContainerTypeImpl extends ThriftPsiCompositeElementImpl implements ThriftContainerType {
+
+  public ThriftContainerTypeImpl(ASTNode node) {
+    super(node);
+  }
+
+  public void accept(@NotNull PsiElementVisitor visitor) {
+    if (visitor instanceof ThriftVisitor) ((ThriftVisitor)visitor).visitContainerType(this);
+    else super.accept(visitor);
+  }
+
+  @Override
+  @Nullable
+  public ThriftListType getListType() {
+    return findChildByClass(ThriftListType.class);
+  }
+
+  @Override
+  @Nullable
+  public ThriftMapType getMapType() {
+    return findChildByClass(ThriftMapType.class);
+  }
+
+  @Override
+  @Nullable
+  public ThriftSetType getSetType() {
+    return findChildByClass(ThriftSetType.class);
+  }
+
+  @Override
+  @Nullable
+  public ThriftTypeAnnotations getTypeAnnotations() {
+    return findChildByClass(ThriftTypeAnnotations.class);
+  }
+
+}
---
+++ gen/com/intellij/plugins/thrift/lang/psi/impl/ThriftGenericTypeImpl.java
@@ -0,0 +1,31 @@
+// This is a generated file. Not intended for manual editing.
+package com.intellij.plugins.thrift.lang.psi.impl;
+
+import java.util.List;
+import org.jetbrains.annotations.*;
+import com.intellij.lang.ASTNode;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiElementVisitor;
+import com.intellij.psi.util.PsiTreeUtil;
+import static com.intellij.plugins.thrift.lang.lexer.ThriftTokenTypes.*;
+import com.intellij.plugins.thrift.lang.psi.*;
+import com.intellij.plugins.thrift.util.ThriftPsiUtil;
+
+public class ThriftGenericTypeImpl extends ThriftPsiCompositeElementImpl implements ThriftGenericType {
+
+  public ThriftGenericTypeImpl(ASTNode node) {
+    super(node);
+  }
+
+  public void accept(@NotNull PsiElementVisitor visitor) {
+    if (visitor instanceof ThriftVisitor) ((ThriftVisitor)visitor).visitGenericType(this);
+    else super.accept(visitor);
+  }
+
+  @Override
+  @NotNull
+  public List<ThriftFieldType> getFieldTypeList() {
+    return PsiTreeUtil.getChildrenOfTypeAsList(this, ThriftFieldType.class);
+  }
+
+}
