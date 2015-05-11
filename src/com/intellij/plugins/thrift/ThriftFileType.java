33fe066eff8473a087d309764319454b0e3b65ea
VISTALL <vistall.valeriy@gmail.com>
2015-05-11 21:33:01 +0300

    use another icon

--- src/com/intellij/plugins/thrift/ThriftFileType.java
+++ src/com/intellij/plugins/thrift/ThriftFileType.java
@@ -5,43 +5,49 @@ import javax.swing.Icon;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import com.intellij.icons.AllIcons;
+import org.mustbe.consulo.apache.thrift.ThriftIcons;
 import com.intellij.openapi.fileTypes.LanguageFileType;
 
 /**
  * Created by fkorotkov.
  */
-public class ThriftFileType extends LanguageFileType {
-  public static final ThriftFileType INSTANCE = new ThriftFileType();
+public class ThriftFileType extends LanguageFileType
+{
+	public static final ThriftFileType INSTANCE = new ThriftFileType();
 
-  @NonNls
-  public static final String DEFAULT_EXTENSION = "thrift";
+	@NonNls
+	public static final String DEFAULT_EXTENSION = "thrift";
 
-  protected ThriftFileType() {
-    super(ThriftLanguage.INSTANCE);
-  }
+	protected ThriftFileType()
+	{
+		super(ThriftLanguage.INSTANCE);
+	}
 
-  @NotNull
-  @Override
-  public String getName() {
-    return ThriftBundle.message("thrift.name");
-  }
+	@NotNull
+	@Override
+	public String getName()
+	{
+		return ThriftBundle.message("thrift.name");
+	}
 
-  @NotNull
-  @Override
-  public String getDescription() {
-    return ThriftBundle.message("thrift.description");
-  }
+	@NotNull
+	@Override
+	public String getDescription()
+	{
+		return ThriftBundle.message("thrift.description");
+	}
 
-  @NotNull
-  @Override
-  public String getDefaultExtension() {
-    return DEFAULT_EXTENSION;
-  }
+	@NotNull
+	@Override
+	public String getDefaultExtension()
+	{
+		return DEFAULT_EXTENSION;
+	}
 
-  @Nullable
-  @Override
-  public Icon getIcon() {
-    return AllIcons.Providers.Apache;
-  }
+	@Nullable
+	@Override
+	public Icon getIcon()
+	{
+		return ThriftIcons.FileTypes.ApacheThrift;
+	}
 }
---
+++ src/org/mustbe/consulo/apache/thrift/ThriftIcons.java
@@ -0,0 +1,15 @@
+package org.mustbe.consulo.apache.thrift;
+
+import javax.swing.Icon;
+
+import com.intellij.openapi.util.IconLoader;
+
+// Generated Consulo DevKit plugin 
+public interface ThriftIcons
+{
+	interface FileTypes
+	{
+		Icon ApacheThrift = IconLoader.getIcon("/icons/fileTypes/apacheThrift.png");  // 16x16
+	}
+
+}
