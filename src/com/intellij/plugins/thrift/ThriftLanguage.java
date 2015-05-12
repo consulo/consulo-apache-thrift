00280a6416fdd414655c909a713858942506f62b
VISTALL <vistall.valeriy@gmail.com>
2015-05-13 01:00:24 +0300

    provide display name

--- src/com/intellij/plugins/thrift/ThriftLanguage.java
+++ src/com/intellij/plugins/thrift/ThriftLanguage.java
@@ -5,10 +5,18 @@ import com.intellij.lang.Language;
 /**
  * Created by fkorotkov.
  */
-public class ThriftLanguage extends Language {
-  public static ThriftLanguage INSTANCE = new ThriftLanguage();
+public class ThriftLanguage extends Language
+{
+	public static ThriftLanguage INSTANCE = new ThriftLanguage();
 
-  protected ThriftLanguage() {
-    super("thrift");
-  }
+	protected ThriftLanguage()
+	{
+		super("thrift");
+	}
+
+	@Override
+	public String getDisplayName()
+	{
+		return "Apache Thrift";
+	}
 }
