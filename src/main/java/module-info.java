23bf9427df171bbc98f4561ce75cd33be1dd80a4
Valery Semenchuk <vistall.valeriy@gmail.com>
2026-04-18 18:51:41 +0300

    drop ide api deps

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,8 +6,29 @@ module com.intellij.plugins.thrift {
 	// TODO remove in future
 	requires java.desktop;
 
+	requires consulo.application.api;
+	requires consulo.code.editor.api;
+	requires consulo.color.scheme.api;
+	requires consulo.component.api;
+	requires consulo.document.api;
+	requires consulo.file.editor.api;
 	requires consulo.ide.api;
-	requires consulo.language.impl;
-	requires consulo.virtual.file.watcher.api;
+	requires consulo.index.io;
+	requires consulo.language.api;
+	requires consulo.language.editor.api;
 	requires consulo.language.editor.impl;
+	requires consulo.language.editor.ui.api;
+	requires consulo.language.impl;
+	requires consulo.localize.api;
+	requires consulo.module.content.api;
+	requires consulo.navigation.api;
+	requires consulo.platform.api;
+	requires consulo.project.api;
+	requires consulo.project.content.api;
+	requires consulo.ui.api;
+	requires consulo.util.collection;
+	requires consulo.util.io;
+	requires consulo.util.lang;
+	requires consulo.virtual.file.system.api;
+	requires consulo.virtual.file.watcher.api;
 }
